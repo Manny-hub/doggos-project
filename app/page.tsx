def82c34701fd811d80f1439307ccd6c027ccb60
Adeyeye Ayomide <77075242+Manny-hub@users.noreply.github.com>
2024-08-15 03:09:42 +0100

    "new commit"

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,50 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  const dogs = [
+    {
+      name: "Gary",
+    },
+    {
+      name: "Hall",
+    },
+    {
+      name: "Marrty",
+    },
+    {
+      name: "Cinge",
+    },
+    {
+      name: "Gerr",
+    },
+    {
+      name: "Marty",
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold font-serif">hey doggos 🐶🐶</h1>
+    <main className="px-10 py-20 p-16 mx-auto max-w-screen-xl">
+      <h1 className="font-serif">Hey Doggos 🐶!</h1>
+      <h2 className="my-12 font-serif">The best doggos in town</h2>
+      <div>
+        {dogs.map(({ name }, idx) => {
+          return (
+            <div key={name}>
+              <div>
+                <h2>{name}</h2>
+              </div>
+              <div>
+                <Image
+                  src={`/images/${idx + 1}. png`}
+                  width={200}
+                  height={160}
+                  alt={name}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
